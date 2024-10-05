163aec97c91c4d6c63ad4400dc5f41a198ed8d3d
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-10-05 22:52:01 +0300

    Issue #18: Create a first-step form to provide an E-mail address

--- personal_assistant/pa_user/urls.py
+++ personal_assistant/pa_user/urls.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordResetConfirmView
 from django.urls import path
 
-from .forms import LoginForm
-from .views import signout
+from .forms import PaUserAuthenticationForm
+from .views import PaUserPasswordResetView, signout
 
 app_name = 'pa_user'
 
@@ -11,10 +11,16 @@ urlpatterns = [
         'login/',
         LoginView.as_view(
             template_name='pa_user/login.html',
-            form_class=LoginForm,
+            form_class=PaUserAuthenticationForm,
             redirect_authenticated_user=True,
         ),
         name='login',
     ),
     path('logout/', signout, name='logout'),
+    path('reset-password/', PaUserPasswordResetView.as_view(), name='reset'),
+    path(
+        'reset-password/confirm/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(),
+        name='reset_confirm',
+    ),
 ]
--- personal_assistant/pa_user/views.py
+++ personal_assistant/pa_user/views.py
@@ -1,8 +1,22 @@
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponsePermanentRedirect
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
+
+from .forms import PaUserPasswordResetForm
+
+
+class PaUserPasswordResetView(PasswordResetView, SuccessMessageMixin):
+    form_class = PaUserPasswordResetForm
+    template_name = 'pa_user/reset.html'
+    email_template_name = 'pa_user/reset_email.html'
+    success_url = reverse_lazy('pa_user:login')
+    success_message = ('An email with instructions to reset your password has '
+                       'been sent to %(email)s.')
 
 
 @login_required
