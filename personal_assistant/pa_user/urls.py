0d66ad916697c4ef12105f3f99741c10b6eb778b
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-10-06 21:29:27 +0300

    Issue #18: Display messages about sending e-mails and updating passwords on the login page instead of using extra steps

--- personal_assistant/pa_user/urls.py
+++ personal_assistant/pa_user/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 
 from .views import (
     PaUserLoginView,
-    PaUserPasswordResetCompleteView as ResetCompleteView,
     PaUserPasswordResetConfirmView as Confirm,
-    PaUserPasswordResetDoneView as PaUserDoneView,
     PaUserPasswordResetView,
     signout
 )
@@ -16,7 +14,5 @@ urlpatterns = [
     path('login/', PaUserLoginView.as_view(), name='login'),
     path('logout/', signout, name='logout'),
     path(url, PaUserPasswordResetView.as_view(), name='reset'),
-    path(f'{url}done/', PaUserDoneView.as_view(), name='done'),
     path(f'{url}confirm/<uidb64>/<token>/', Confirm.as_view(), name='confirm'),
-    path(f'{url}complete/', ResetCompleteView.as_view(), name='complete'),
 ]
