93c2c8d6a409aa79ccf97de24073c52870157820
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-10-12 00:42:04 +0300

    Issue #6: Refactor forms

--- personal_assistant/pa_contacts/forms.py
+++ personal_assistant/pa_contacts/forms.py
@@ -1,57 +1,59 @@
-from django.forms import ModelForm, CharField, EmailField, TextInput, DateField, DateInput
+from datetime import datetime
+from re import sub
+
+from django.forms import ModelForm, CharField, EmailField, TextInput, \
+    DateField, DateInput
 from django.core.exceptions import ValidationError
-from phonenumbers import parse, is_valid_number, NumberParseException, format_number, PhoneNumberFormat
+from phonenumbers import parse, is_valid_number, NumberParseException, \
+    format_number, PhoneNumberFormat
 
 from .models import Contact
 
-import datetime
-import re
+
+PHONE = '+380776665544'
 
 
 class ContactsForm(ModelForm):
-    current_year = datetime.datetime.now().year
+    current_year = datetime.now().year
     name = CharField(
-        min_length=1, 
-        max_length=50, 
-        required=True, 
+        min_length=1,
+        max_length=50,
+        required=True,
         widget=TextInput(attrs={'class': 'form-control'})
     )
     address = CharField(
-        min_length=10, 
-        max_length=150, 
-        required=False, 
+        min_length=10,
+        max_length=150,
+        required=False,
         widget=TextInput(attrs={'class': 'form-control'})
     )
     phone = CharField(
-        min_length=10, 
-        max_length=20, 
-        required=True, 
-        widget=TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': '+380776665544',
-        })
+        min_length=10,
+        max_length=20,
+        required=True,
+        widget=TextInput({'class': 'form-control', 'placeholder': PHONE})
     )
     email = EmailField(
-        min_length=5, 
-        max_length=50, 
-        required=False, 
+        min_length=5,
+        max_length=50,
+        required=False,
         widget=TextInput(attrs={
             'class': 'form-control',
-            'type': 'email',  
-            'id': 'email', 
+            'type': 'email',
+            'id': 'email',
             'name': 'email',
             'placeholder': 'example@example.com',
         })
     )
     birthday = DateField(
-        required=False, 
+        required=False,
         widget=DateInput(attrs={'class': 'form-control', 'type': 'date'})
     )
 
     class Meta:
         model = Contact
         fields = ['name', 'address', 'phone', 'email', 'birthday']
-        
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
@@ -61,29 +63,23 @@ class ContactsForm(ModelForm):
                 self.fields[field].widget.attrs['class'] += ' form-control'
 
     def clean_phone(self):
-        phone = self.cleaned_data.get('phone')
+        if phone := self.cleaned_data.get('phone'):
+            phone = sub(r'(?<!^)\D+', '', phone)
 
-        if phone:
-            phone = re.sub(r'(?<!^)\D+', '', phone)
+            incorrect = ValidationError(
+                'Please enter your phone number in the international format '
+                f'(e.g., {PHONE}).',
+            )
 
             try:
-                if phone.startswith('+'):
-                    parsed_phone = parse(phone, None)  
-                else:
-                    parsed_phone = parse(phone, 'UA')
+                parsed = parse(phone, None if phone.startswith('+') else 'UA')
 
-                if not is_valid_number(parsed_phone):
-                    raise ValidationError(
-                        'Please enter your phone number in the international format (e.g., +380776665544).'
-                    )
+                if not is_valid_number(parsed):
+                    raise incorrect
 
+                return format_number(parsed, PhoneNumberFormat.E164)
 
-                formatted_phone = format_number(parsed_phone, PhoneNumberFormat.E164)
-                return formatted_phone
-
-            except NumberParseException as e:
-                raise ValidationError(
-                    'Please enter your phone number in the international format (e.g., +380776665544).'
-                )
+            except NumberParseException:
+                raise incorrect
 
         return phone
