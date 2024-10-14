6ef89054effdb662ab890fc302fc9a72138a04d3
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-10-14 17:23:02 +0300

    Issue #11, #12, #25: Fix coding standards

--- personal_assistant/pa_contacts/views.py
+++ personal_assistant/pa_contacts/views.py
@@ -80,9 +80,9 @@ def main(request):
             'upcoming_birthdays': sorted_contacts,
             'days': days,
             'tabs': tabs,
-            'icon': 'person-fill',
         },
-        title='Contacts',
+        'person-fill',
+        'Contacts',
     )
 
 
