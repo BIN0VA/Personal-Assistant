78aa60eddd0721d15d62ffcefc630c9756e9456a
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-10-14 19:13:57 +0300

    Issue #16: Send letters to super users when a new user is created and use HTML format for pending letters

--- personal_assistant/pa_user/signals.py
+++ personal_assistant/pa_user/signals.py
@@ -1,9 +1,38 @@
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.core.mail import EmailMultiAlternatives, send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
+
+
+def send(
+    sender: User,
+    template: str,
+    status: str = 'pending admin approval',
+    receiver: User = None
+) -> None:
+    email = EmailMultiAlternatives(
+        f'Account details for {sender.username} at {settings.PROJECT_NAME} '
+        f'({status})',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=[(receiver or sender).email],
+    )
+
+    email.content_subtype = 'html'
+
+    message = render_to_string(
+        f'pa_user/register/email/{template}.html',
+        {
+            'user': receiver or sender,
+            'guest': sender if receiver else None,
+            'project_name': settings.PROJECT_NAME,
+        },
+    )
+
+    email.attach_alternative(message, 'text/html')
+
+    email.send()
 
 
 @receiver(post_save, sender=User)
@@ -13,23 +42,11 @@ def welcome(
     created: bool,
     **kwargs: dict
 ) -> None:
-    if not created:
-        return
+    if created:
+        send(instance, 'guest')
 
-    subject = (
-        f'Account details for {instance.username} at {settings.PROJECT_NAME} '
-        '(pending admin approval)'
-    )
-
-    message = (
-        f'Thank you for registering at {settings.PROJECT_NAME}. Your '
-        'application for an account is currently pending approval. Once it '
-        'has been approved, you will receive another email containing '
-        'information about how to log in, set your password, and other '
-        'details.'
-    )
-
-    send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [instance.email])
+        for user in User.objects.filter(is_active=True, is_superuser=True):
+            send(instance, 'admin', receiver=user)
 
 
 before = {}
@@ -48,31 +65,10 @@ def activation(
     created: bool,
     **kwargs: dict
 ) -> None:
-    if not (
+    if (
         not created and
         not before.get(instance.pk) and
         instance.is_active and
         not instance.last_login
     ):
-        return
-
-    email = EmailMultiAlternatives(
-        f'Account details for {instance.username} at {settings.PROJECT_NAME} '
-        '(approved)',
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        to=[instance.email],
-    )
-
-    email.content_subtype = 'html'
-
-    message = render_to_string(
-        'pa_user/register/email.html',
-        {
-            'user': instance,
-            'project_name': settings.PROJECT_NAME,
-        },
-    )
-
-    email.attach_alternative(message, 'text/html')
-
-    email.send()
+        send(instance, 'activation', 'approved')
