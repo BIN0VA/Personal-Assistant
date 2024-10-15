fd2e8fca4a1d034a1e3c2ebd1521464a136efe99
Oleksandr Horbatiuk <oleksandr.v.horbatiuk@gmail.com>
2024-10-15 16:04:02 +0300

    Issue #44: Add a footer with a copyright section

--- personal_assistant/pa_core/context_processors.py
+++ personal_assistant/pa_core/context_processors.py
@@ -4,6 +4,15 @@ from django.urls import reverse
 
 
 def global_context(request: WSGIRequest) -> dict:
+    teammates = {
+        'B': 'derso001',
+        'I': 'DeTr1ll',
+        'N': 'sholohova27',
+        'O': 'lexhouk',
+        'V': 'mzLeVit',
+        'A': 'alina-kylymnyk',
+    }
+
     entity_types = ('contacts', 'notes', 'tags')
 
     if request.GET.get('query'):
@@ -23,6 +32,11 @@ def global_context(request: WSGIRequest) -> dict:
 
     return {
         'project_name': settings.PROJECT_NAME,
+        'project_team': ''.join([
+            f'<a href="https://github.com/{nickname}" target="_blank">{letter}'
+            '</a>'
+            for letter, nickname in teammates.items()
+        ]),
         'search': {
             current_entity_type: current_entity_type == active_entity_type
             for current_entity_type in entity_types
